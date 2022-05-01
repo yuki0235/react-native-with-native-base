c05cc147b98118258e979e368df0403ac3a86ece
Nyakan1sh1 <nyakanishi.harry@gmail.com>
2022-05-01 12:12:32 +0900

    Add Custom Color Theme

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
 import React from 'react'
-import { NativeBaseProvider, Text, Box } from 'native-base'
+import { extendTheme, NativeBaseProvider, Text, Box } from 'native-base'
+
+const newColorTheme = {
+    brand: {
+        900: '#8287af',
+        800: '#7c83db',
+        700: '#b3bef6',
+    },
+}
+const theme = extendTheme({ colors: newColorTheme })
 
 export default function App() {
     return (
-        <NativeBaseProvider>
+        <NativeBaseProvider theme={theme}>
             <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
                 <Text>This is React Native App With Native Base</Text>
             </Box>
